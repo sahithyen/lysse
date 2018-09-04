015133364cf0c7b9615ccad19872ef3bf48146b5
Sahithyen Kanaganayagam <sahithyen@exygen.systems>
2018-09-04 19:19:17 +0200

    Add header and argument parameters

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
+/**
+ * @brief Entry point
+ * @author Sahithyen Kanaganayagam
+ * @date 30 Aug 2018
+ *
+ * This is the entry code of the Lysse compiler. It does process the input and
+ * options choosen by the user and runs (parts of) the compiler accordingly.
+ */
+
 #include <stdio.h>
 
-int main() {
-	printf("Lysse compiler");
-	return 0;
+int main(int argc, char **argv) {
+  printf("Lysse compiler");
+  return 0;
 }

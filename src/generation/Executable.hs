de367bd3fc9762cb0b07b63415ef7681b3c50bc8
Sahithyen Kanaganayagam <mail@sahithyen.com>
2023-03-17 15:56:54 +0000

    Implement relocatable writer

--- src/generation/Executable.hs
+++ src/generation/Executable.hs
@@ -1,80 +1,29 @@
 module Executable (generate, RelocationTable) where
 
-import Data.Binary (Word16, Word32)
+import Data.Binary (Word32, Word8, putWord8)
 import Data.Binary.Put (PutM, putWord32le)
-import DataWriter (offsetPutData)
-import Elf (ELFHeaderParameter (ELFHeaderParameter), ELFProgramHeaderParameter (ELFProgramHeaderParameter), elfHeader, elfProgramHeader)
-import Instructions (adr, ldrlx, mov, movzw, movzx, msub, r0, r1, r2, r3, r8, sdiv, stri, svc)
-import Relocation (RelocationTable, joinRelocationTable, offsetRelocations)
-import RoutineWriter (RoutineWriter, addDWord, addString, assembleRoutine, dataTable, programSize, relocate, relocationTable)
+import Relocation (Relocatable (..), RelocatableWriter, RelocationTable, addRelocatable, executeRelocatableWriter)
 
-putRoutine :: [Word32] -> PutM ()
-putRoutine [] = pure ()
-putRoutine (x : xs) = do
-  putWord32le x
-  putRoutine xs
+addWord :: Word32 -> Relocatable
+addWord v = Relocatable (const $ putWord32le v) 4 4
+
+addByte :: Word8 -> Relocatable
+addByte v = Relocatable (const $ putWord8 v) 1 1
+
+routine :: RelocatableWriter ()
+routine = do
+  addRelocatable "code" (addWord 20)
+  addRelocatable "code" (addByte 10)
+  addRelocatable "code" (addByte 0xff)
+  addRelocatable "code" (addByte 0xfe)
+  addRelocatable "code" (addByte 0xfd)
+  addRelocatable "code" (addByte 0xfc)
+  addRelocatable "code" (addWord 20)
+
+  addRelocatable "cata" (addWord 100)
 
 generate :: Data.Binary.Put.PutM ()
 generate = do
-  elfHeader (ELFHeaderParameter executionEntryAddress programHeadersEntrySize programHeadersCount)
-  elfProgramHeader (ELFProgramHeaderParameter programFileOffset loadAddress loadAddress fullSize fullSize)
-  putRoutine program
-  putData
+  putCode rt
   where
-    elfHeaderSize = 0x40
-    programHeadersEntrySize = 0x38
-    programHeadersCount = 1
-    programHeadersSize = programHeadersEntrySize * programHeadersCount
-    headerSize = fromIntegral (elfHeaderSize + programHeadersSize)
-    (routineState, relocatableProgram) = assembleRoutine mainRoutine
-    programFileSize = headerSize + programSize routineState
-    programRt = offsetRelocations headerSize (relocationTable routineState)
-    (putData, dataRt, dataSize) = offsetPutData programFileSize (dataTable routineState)
-    fullRt = joinRelocationTable programRt dataRt
-    fullSize = programFileSize + dataSize
-    program = relocate relocatableProgram fullRt
-    programFileOffset = 0 -- Offset in file needs to be a multiple of the page size (page size in arm: 4096 bytes)
-    loadAddress = 0
-    executionEntryAddress = loadAddress + headerSize
-
--- r0 exit code
-exitRoutine :: RoutineWriter ()
-exitRoutine = do
-  movzw r8 93
-  svc 0
-
-printRoutine :: String -> RoutineWriter ()
-printRoutine str = do
-  (lab, len) <- addString str
-  movzx r0 1
-  adr r1 lab
-  movzx r2 (fromIntegral len)
-  movzx r8 64
-  svc 0
-
--- r0 dividend
--- r1 divisor
--- r2 quotient
--- r3 remainder
-rdivRoutine :: RoutineWriter ()
-rdivRoutine = do
-  sdiv r2 r0 r1
-  msub r3 r2 r1 r0
-
-mainRoutine :: RoutineWriter ()
-mainRoutine = do
-  addDWord "buf" 200
-
-  -- Calculate 20 / 8
-  movzx r0 20
-  movzx r1 7
-  rdivRoutine
-  mov r0 r3
-  -- 6
-
-  adr r1 "buf"
-  -- stri r3 r1 0
-
-  ldrlx r0 "buf"
-
-  exitRoutine
+    (putCode, rt, _) = executeRelocatableWriter routine
--- src/generation/Relocation.hs
+++ src/generation/Relocation.hs
@@ -1,12 +1,136 @@
-module Relocation (RelocationTable, offsetRelocations, joinRelocationTable) where
+module Relocation (RelocationTable, offsetRelocations, joinRelocationTables, RelocatableWriter, Relocatable (..), addRelocatable, executeRelocatableWriter) where
 
-import Data.Binary (Word64)
-import Data.Map (Map, union)
+import Control.Exception (assert)
+import Control.Monad.State (State, execState, gets, modify)
+import Data.Binary (Put, Word64, Word8, putWord8)
+import Data.List (sortOn)
+import Data.Map (Map, empty, insert, lookup, mapAccum, union)
 
+-- Relocation table
 type RelocationTable = Map String Word64
 
 offsetRelocations :: Word64 -> RelocationTable -> RelocationTable
 offsetRelocations a = fmap (+ a)
 
-joinRelocationTable :: RelocationTable -> RelocationTable -> RelocationTable
-joinRelocationTable = union
+joinRelocationTables :: RelocationTable -> RelocationTable -> RelocationTable
+joinRelocationTables = union
+
+-- Relocatable
+data Relocatable = Relocatable {relocatableData :: RelocationTable -> Put, reloctableSize :: Word64, relocatableAlignment :: Word64}
+
+emptyRelocatable :: Relocatable
+emptyRelocatable = Relocatable (const $ pure ()) 0 1
+
+putRelocatable :: Relocatable -> RelocationTable -> Put
+putRelocatable (Relocatable d _ _) = d
+
+pad :: Word8 -> Word64 -> Put
+pad _ 0 = pure ()
+pad v n = do
+  pad v (n - 1)
+  putWord8 v
+
+joinRelocatableAligned :: Word64 -> Relocatable -> Relocatable -> (Relocatable, Word64)
+joinRelocatableAligned na (Relocatable ad as aa) (Relocatable bd bs ba) = (crel, off)
+  where
+    bfa = lcm na ba
+    off = if as `mod` bfa == 0 then 0 else bfa - (as `mod` bfa)
+    cd rt = do
+      ad rt
+      pad 0 off
+      bd rt
+    cs = as + off + bs
+    biga = max aa bfa
+    smalla = min aa bfa
+    ca = assert (biga `mod` smalla == 0) biga
+    crel = Relocatable cd cs ca
+
+joinRelocatable :: Relocatable -> Relocatable -> (Relocatable, Word64)
+joinRelocatable = joinRelocatableAligned 1
+
+-- RelocatableUnit
+data RelocatableUnit = RelocatableUnit Relocatable RelocationTable
+
+emptyRelocatableUnit :: RelocatableUnit
+emptyRelocatableUnit = RelocatableUnit emptyRelocatable empty
+
+addRelocatableToUnit :: RelocatableUnit -> Relocatable -> RelocatableUnit
+addRelocatableToUnit (RelocatableUnit rela rt) relb = RelocatableUnit (fst $ joinRelocatable rela relb) rt
+
+joinAlignedUnits :: Word64 -> RelocatableUnit -> RelocatableUnit -> RelocatableUnit
+joinAlignedUnits alignment (RelocatableUnit ar art) (RelocatableUnit br brt) = RelocatableUnit cr crt
+  where
+    as = reloctableSize ar
+    (cr, off) = joinRelocatableAligned alignment ar br
+    boff = as + off
+    crt = joinRelocationTables art (offsetRelocations boff brt)
+
+joinUnits :: RelocatableUnit -> RelocatableUnit -> RelocatableUnit
+joinUnits = joinAlignedUnits 1
+
+joinUnitList :: [RelocatableUnit] -> RelocatableUnit
+joinUnitList us = foldr joinUnits emptyRelocatableUnit sus
+  where
+    sus = sortOn (\(RelocatableUnit (Relocatable _ s _) _) -> s) us
+
+relocatableUnitSize :: RelocatableUnit -> Word64
+relocatableUnitSize (RelocatableUnit rel _) = reloctableSize rel
+
+-- Segment
+newtype Segment = Segment [RelocatableUnit]
+
+addRelocatableToSegment :: Relocatable -> Segment -> Segment
+addRelocatableToSegment rel (Segment []) = Segment [RelocatableUnit rel empty]
+addRelocatableToSegment rel (Segment (u : us)) = Segment (addRelocatableToUnit u rel : us)
+
+newtype RelocatableWriterState = State {segments :: Map String Segment}
+
+setSegment :: String -> Segment -> RelocatableWriterState -> RelocatableWriterState
+setSegment sname segment state = state {segments = insert sname segment ss}
+  where
+    ss = segments state
+
+initialRelocatableWriterState :: RelocatableWriterState
+initialRelocatableWriterState = State empty
+
+joinSegmentUnits :: Segment -> RelocatableUnit
+joinSegmentUnits (Segment us) = joinUnitList us
+
+type RelocatableWriter = State RelocatableWriterState
+
+modifySegment :: String -> (Segment -> Segment) -> RelocatableWriter ()
+modifySegment sname fn = do
+  segments <- gets segments
+
+  segment <- case Data.Map.lookup sname segments of
+    Just segment -> do
+      return (fn segment)
+    Nothing -> do
+      return (fn (Segment []))
+
+  modify (setSegment sname segment)
+  return ()
+
+addRelocatable :: String -> Relocatable -> RelocatableWriter ()
+addRelocatable sname rel = do
+  modifySegment sname $ addRelocatableToSegment rel
+  return ()
+
+type SegmentTable = Map String Word64
+
+assembleNamedUnits :: Word64 -> Map String RelocatableUnit -> (RelocatableUnit, SegmentTable)
+assembleNamedUnits alignment =
+  mapAccum
+    ( \acc u ->
+        ( joinAlignedUnits alignment acc u,
+          relocatableUnitSize acc
+        )
+    )
+    emptyRelocatableUnit
+
+executeRelocatableWriter :: RelocatableWriter () -> (RelocationTable -> Put, RelocationTable, SegmentTable)
+executeRelocatableWriter fn = (putRelocatable rel, rt, st)
+  where
+    (State segmentsMap) = execState fn initialRelocatableWriterState
+    namedUnits = fmap joinSegmentUnits segmentsMap
+    (RelocatableUnit rel rt, st) = assembleNamedUnits 4096 namedUnits

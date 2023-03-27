69380b4838adef743290f71d8e02309ded537b54
Sahithyen Kanaganayagam <mail@sahithyen.com>
2023-03-27 14:51:47 +0000

    Add cmd line interface

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,25 +1,68 @@
 module Lib
-  ( compile,
+  ( entry,
   )
 where
 
+import Control.Monad (when)
 import Data.Binary.Put (runPut)
 import Data.ByteString.Lazy as B (writeFile)
 import Elf (generateElf)
 import LyGen (lysseProgram)
 import LyParser (parseLy)
+import STree (showStatements)
+import System.Console.ArgParser
+  ( CmdLnInterface,
+    Descr (Descr),
+    ParserSpec,
+    andBy,
+    boolFlag,
+    mkApp,
+    parsedBy,
+    reqPos,
+    runApp,
+    setAppDescr,
+  )
 import System.IO (IOMode (ReadMode), hGetContents, openFile)
 
-compile :: IO ()
-compile = do
+data LycArgs = Args {source :: String, output :: String, showSt :: Bool}
+
+lycArgsParser :: ParserSpec LycArgs
+lycArgsParser =
+  Args
+    `parsedBy` reqPos "source"
+    `Descr` "Path to input Lysse code"
+    `andBy` reqPos "output"
+    `Descr` "Output path of the generated executable"
+    `andBy` boolFlag "show-syntax-tree"
+    `Descr` "Prints the syntax tree generated from the input Lysse code"
+
+lycInterface :: IO (CmdLnInterface LycArgs)
+lycInterface =
+  (`setAppDescr` "Compiles lysse code to a Linux executable (aarch64)")
+    <$> mkApp lycArgsParser
+
+entry :: IO ()
+entry = do
+  interface <- lycInterface
+  runApp interface compile
+
+compile :: LycArgs -> IO ()
+compile args = do
   -- Read
-  handle <- openFile "code.ly" ReadMode
+  let sourceFile = source args
+  handle <- openFile sourceFile ReadMode
   contents <- hGetContents handle
 
   -- Parse
-  let newSt = parseLy contents
+  let st = parseLy contents
 
-  case newSt of
+  when (showSt args) $ case st of
+    Right ss -> do
+      putStrLn "syntax tree:"
+      putStr $ showStatements ss
+    _ -> putStrLn "unable to show syntax tree"
+
+  case st of
     Left e ->
       do
         putStrLn e
@@ -28,4 +71,4 @@ compile = do
         -- Generate
         let program = lysseProgram ss
         let elf = generateElf program
-        B.writeFile "ly" (runPut elf)
+        B.writeFile (output args) (runPut elf)

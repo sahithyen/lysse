17de0ce718002aa16f3b07e45256d81d77cb5f6e
Sahithyen Kanaganayagam <mail@sahithyen.com>
2023-03-27 13:49:34 +0000

    Remove old parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,48 +6,18 @@ where
 import Data.Binary.Put (runPut)
 import Data.ByteString.Lazy as B (writeFile)
 import Elf (generateElf)
-import Lexer (llex)
 import LyGen (lysseProgram)
 import LyParser (parseLy)
-import OldParser (parse)
-import STree (showStatements)
 import System.IO (IOMode (ReadMode), hGetContents, openFile)
 
 compile :: IO ()
 compile = do
+  -- Read
   handle <- openFile "code.ly" ReadMode
   contents <- hGetContents handle
 
-  putStrLn "-- Old parser"
-  let tokens = llex contents
-  let st = parse tokens
-  either
-    ( \e -> do
-        putStrLn "No tree to show"
-        print e
-    )
-    ( \(stmts, _) -> do
-        putStrLn "Tree:"
-        putStrLn $ showStatements stmts
-    )
-    st
-
-  putStrLn ""
-
-  putStrLn "-- New parser"
+  -- Parse
   let newSt = parseLy contents
-  either
-    ( \e -> do
-        putStrLn "No tree to show"
-        putStrLn e
-    )
-    ( \stmts -> do
-        putStrLn "Tree:"
-        putStrLn $ showStatements stmts
-    )
-    newSt
-
-  putStrLn ""
 
   case newSt of
     Left e ->
@@ -55,6 +25,7 @@ compile = do
         putStrLn e
     Right ss ->
       do
+        -- Generate
         let program = lysseProgram ss
         let elf = generateElf program
         B.writeFile "ly" (runPut elf)

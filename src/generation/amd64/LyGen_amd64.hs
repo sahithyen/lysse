c812336ef342350fe913904cf46ac6b4178449b3
Sahithyen Kanaganayagam <mail@sahithyen.com>
2023-04-03 08:57:10 +0000

    Begin x86 print char impl

--- src/generation/amd64/LyGen_amd64.hs
+++ src/generation/amd64/LyGen_amd64.hs
@@ -3,11 +3,14 @@ module LyGen_amd64 (lysseProgramAmd64) where
 import Code_amd64 (label)
 import Macros_amd64 (exitMacroAmd64, printMacro)
 import Relocation (RelocatableWriter)
+import Routines_amd64 (printChar)
 import STree (LAStatement)
 
 lysseProgramAmd64 :: [LAStatement] -> RelocatableWriter ()
 lysseProgramAmd64 _ = do
   label "_start"
   printMacro "hello, world\n"
+
+  printChar
 
   exitMacroAmd64 0
--- src/generation/amd64/Routines_amd64.hs
+++ src/generation/amd64/Routines_amd64.hs
@@ -1,1 +1,20 @@
-module Routines_amd64 () where
+module Routines_amd64 (printChar) where
+
+import Code_amd64 (label)
+import Data (addDWord)
+import Instructions_amd64 (Register (RA, RD, RDI, RSI), leaRipRel, movImm, syscall)
+import Relocation (RelocatableWriter)
+
+-- RDI = ascii char
+printChar :: RelocatableWriter ()
+printChar = do
+  label "printChar"
+  buf <- addDWord 0
+  leaRipRel RSI buf
+  --
+
+  movImm RA 1
+  movImm RDI 1
+
+  movImm RD 1
+  syscall
